bf7afc6a775185a42b78b1419304b9883afa6625
Julian Lore <julian.lore@mail.mcgill.ca>
2024-07-03 20:11:29 -0400

    Add basic neovim options

--- neovim.nix
+++ neovim.nix
@@ -12,4 +12,22 @@ in
 
   programs.nixvim.enable = true;
   programs.nixvim.defaultEditor = true;
+  programs.nixvim.viAlias = true;
+  programs.nixvim.vimAlias = true;
+  programs.nixvim.opts = {
+    hlsearch = false;
+    number = true;
+    relativenumber = true;
+    breakindent = true;
+    undofile = true;
+    mouse = "";
+    # Case insensitive searching UNLESS /C or capital in search
+    ignorecase = true;
+    smartcase = true;
+    termguicolors = true;
+    tabstop = 2;
+    shiftwidth = 2;
+    # tab = spaces
+    expandtab = true;
+  };
 }
