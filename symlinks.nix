57099883bf69437698b6627c1f7c8f8ae28e328d
Julian Lore <julian.lore@mail.mcgill.ca>
2024-07-06 01:37:27 -0400

    Use path instead of making another symlink

--- symlinks.nix
+++ symlinks.nix
@@ -1,15 +1,15 @@
-{ config, osConfig, ... }:
+{ osConfig, ... }:
 let data-prefix = "data/home";
 files = [
   ".config/syncthing"
 ];
 # Lambda which takes a filename and returns an attribute set of name and value with the source being a symlink to the same file but inside the data-prefix
-# i.e. sl test.txt => { name = "test.txt", value = { source = config.lib.file.mkOutOfStoreSymlink "/home/jl/data/home/test.txt" }; }
-# When calling listToAttrs on a list of these attribute sets, we get an attribute set with home.file."${filename}".source = config.lib.file.mkOutOfStoreSymlink "/home/jl/data/${filename}"
+# i.e. sl test.txt => { name = "test.txt", value = { source = /home/jl/data/home/test.txt }; }
+# When calling listToAttrs on a list of these attribute sets, we get an attribute set with home.file."${filename}".source = /home/jl/data/${filename}
 sl = filename: {
   name = filename;
   value = {
-    source = (config.lib.file.mkOutOfStoreSymlink (builtins.concatStringsSep "/" [osConfig.home-manager.users.jl.home.homeDirectory data-prefix filename]));
+    source = /${osConfig.home-manager.users.jl.home.homeDirectory}/${data-prefix}/${filename};
   };
 }; in
 {
