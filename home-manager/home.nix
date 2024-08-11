358365afe8f347fe9c3399b3aa377ebc028110dc
Julian Lore <julian.lore@mail.mcgill.ca>
2024-08-11 07:43:40 -0400

    Remove comments from generated home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -44,31 +44,6 @@
   programs.nixvim = import ./neovim { pkgs = pkgs; plugin-telescope-recent-files = plugin-telescope-recent-files; };
   programs.fish = import ./fish { pkgs = pkgs; plugin-fish-abbreviation-tips = plugin-fish-abbreviation-tips; };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jl/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     # Used by nh
     FLAKE = "$HOME/NixOS";
