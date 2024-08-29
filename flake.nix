d1e6c78f63d3850477007a1c7188139856d7f5a8
Julian Lore <julian.lore@mail.mcgill.ca>
2024-08-28 20:18:39 -0400

    Add firewall rules for wireguard using NetworkManager

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        ./rpfilter-wireguard-networkmanager.nix
         ./graphical.nix
         ./hosts/JollyJoy
         jovian.nixosModules.default
