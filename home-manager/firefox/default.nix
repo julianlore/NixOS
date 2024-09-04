c6e0f327ad5e74f090e83f320228ecd0657bd44a
Julian Lore <julian.lore@mail.mcgill.ca>
2024-09-03 20:11:05 -0400

    Set home Firefox package to null to avoid overwriting firejail wrapper

--- home-manager/firefox/default.nix
+++ home-manager/firefox/default.nix
@@ -1,4 +1,8 @@
 {
   imports = [ ./arkenfox.nix ./userchrome.nix ];
-  programs.firefox = { enable = true; };
+  programs.firefox = {
+    enable = true;
+    # Do not install firefox for user, as it will overwrite the firejail wrapper
+    package = null;
+  };
 }
