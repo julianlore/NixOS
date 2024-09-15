0c9018c139a452127f8b8223eac9b72878404482
Julian Lore <julian.lore@mail.mcgill.ca>
2024-09-15 02:31:53 -0400

    Persist /var/lib/rfkill

--- impermanence.nix
+++ impermanence.nix
@@ -6,6 +6,7 @@
       "/var/log"
       "/var/lib/nixos"
       "/var/lib/bluetooth"
+      "/var/lib/rfkill" # Remember which devices (wifi, bluetooth) are off
       "/var/lib/systemd/coredump"
       "/etc" # For mutable passwords/without hardcoding hashedPassword (couldn't get hashedPasswordFile to work)
       "/home"
