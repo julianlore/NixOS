2a684b8e962e3ccf71556cffe75f4627208ba8e1
Julian Lore <julian.lore@mail.mcgill.ca>
2025-05-19 16:27:11 -0400

    Change Arkenfox historyFormDataAndDownloads to browsingHistoryAndDownloads (FF136+)

--- home-manager/firefox/arkenfox.nix
+++ home-manager/firefox/arkenfox.nix
@@ -7,12 +7,13 @@
       version = "master";
     };
 
+    # See output of `nix build "github:dwarfmaster/arkenfox-nixos#arkenfox-vmaster-doc-static"` for options
     profiles.Default.arkenfox = {
       enable = true;
       enableAllSections = true;
       # Resume previous session
       "0100"."0102"."browser.startup.page".value = 3;
-      "2800"."2811"."privacy.clearOnShutdown_v2.historyFormDataAndDownloads".value =
+      "2800"."2812"."privacy.clearOnShutdown_v2.browsingHistoryAndDownloads".value =
         false;
     };
   };
