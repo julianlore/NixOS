59f8c307e85a6493de62f17aeba820a97ae37a52
Julian Lore <julian.lore@mail.mcgill.ca>
2025-08-24 19:19:38 -0400

    Add firewall rules to redirect ports 80 and 443 to unprivileged ports

--- podman.nix
+++ podman.nix
@@ -11,4 +11,13 @@
   };
 
   environment.systemPackages = with pkgs; [ podman-compose ];
+
+  # Redirect incoming (PREROUTING) and outgoing (OUTPUT) port 80 traffic to port 8080 to allow hosting services on port 8080 without privileged access
+  # Same for port 443 to 8443
+  networking.firewall.extraCommands = ''
+    iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+    iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 8443
+    iptables -t nat -A OUTPUT --dst 127.0.0.1 -p tcp --dport 80 -j REDIRECT --to-port 8080
+    iptables -t nat -A OUTPUT --dst 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to-port 8443
+  '';
 }
