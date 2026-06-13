add21e8efc30f83b8b7086b72ffbad6a9f5dab4f
Julian Lore <julian.lore@mail.mcgill.ca>
2026-06-13 17:50:09 -0400

    Remove nixvim.inputs.nixpkgs.follows
    It is now set by
    home-manager.users.<user>.programs.nixvim.nixpkgs.source

--- flake.nix
+++ flake.nix
@@ -20,7 +20,6 @@
     };
     nixvim = {
       url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
